b9e993294a8230c8e55242af07695832b2ec4c2a
Fabrício de Medeiros <gannba@hotmail.com>
2025-06-28 23:24:56 -0300

    Update alunos.js

--- js/alunos.js
+++ js/alunos.js
@@ -1,1 +1,39 @@
+async function carregarAlunos() {
+  const token = localStorage.getItem("token");
 
+  if (!token) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const resposta = await fetch("https://fasil.criarsite.online/api/alunos/", {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + token,
+      "Content-Type": "application/json"
+    }
+  });
+
+  const dados = await resposta.json();
+  console.log(dados);
+
+  if (resposta.ok) {
+    const lista = document.getElementById("listaAlunos");
+    dados.alunos.forEach(aluno => {
+
+      const li = document.createElement("li");
+      li.innerHTML = `<strong>Usuário</strong>: ${aluno.id_user} - ${aluno.user}`;
+      lista.appendChild(li);
+    });
+  } else {
+    document.getElementById("mensagem").textContent = dados.error || "Erro ao carregar alunos.";
+    if (resposta.status === 401) window.location.href = "index.html";
+  }
+}
+
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "index.html";
+}
+
+carregarAlunos();
